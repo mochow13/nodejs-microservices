8d270f924a1332e8bb1f6ca8e3175e38f794dfb4
mochow13 <md.mottakin.chowdhury@gmail.com>
2019-05-19 03:05:15 +0600

    add register, unregister, cleanup, getservice functionalities, dummy endpoints, heartbeat mechanism

---
+++ service-registry/server/lib/ServiceRegistry.js
@@ -0,0 +1,61 @@
+const semver = require('semver');
+
+class ServiceRegistry {
+    constructor(log) {
+        this.log = log;
+        this.services = {};
+        this.timeout = 30;
+    }
+
+    get(name, version) {
+        this.cleanup();
+        const candidates = Object.values(this.services)
+            .filter(service => service.name === name && semver.satisfies(service.version, version));
+
+        return candidates[Math.floor(Math.random() * candidates.length)];
+    }
+
+    register(name, version, ip, port) {
+        this.cleanup();
+        const key = name + version + ip + port;
+
+        if (!this.services[key]) {
+            this.services[key] = {
+                timestamp: Math.floor(new Date() / 1000),
+                ip,
+                port,
+                name,
+                version,
+            };
+            // this.services[key] = {};
+            // this.services[key].timestamp = Math.floor(new Date() / 1000);
+            // this.services[key].ip = ip;
+            // this.services[key].port = port;
+            // this.services[key].name = name;
+            // this.services[key].version = version;
+            this.log.debug(`Service added ${name}, version ${version} at ${ip}:${port}`);
+            return key;
+        }
+        this.services[key].timestamp = Math.floor(new Date() / 1000);
+        this.log.debug(`Service updated ${name}, version ${version} at ${ip}:${port}`);
+        return key;
+    }
+
+    unregister(name, version, ip, port) {
+        const key = name + version + ip + port;
+        delete this.services[key];
+        return key;
+    }
+
+    cleanup() {
+        const now = Math.floor(new Date() / 1000);
+        Object.keys(this.services).forEach((key) => {
+            if (this.services[key].timestamp + this.timeout < now) {
+                delete this.services[key];
+                this.log.debug(`Removed service ${key}`);
+            }
+        });
+    }
+}
+
+module.exports = ServiceRegistry;
