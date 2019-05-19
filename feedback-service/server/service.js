f0db4bfb0f53c0471c51ca48db3b006cd3e8108b
mochow13 <md.mottakin.chowdhury@gmail.com>
2019-05-19 15:51:10 +0600

    add feedback service

---
+++ feedback-service/server/service.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const Feedback = require('./lib/Feedback');
+
+const service = express();
+
+module.exports = (config) => {
+  const feedback = new Feedback(config.data.feedback);
+
+  const log = config.log();
+  // Add a request logging middleware in development mode
+  if (service.get('env') === 'development') {
+    service.use((req, res, next) => {
+      log.debug(`${req.method}: ${req.url}`);
+      return next();
+    });
+  }
+
+  service.get('/list', async (req, res, next) => {
+    try {
+      return res.json(await feedback.getList());
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  service.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    // Log out the error to the console
+    log.error(error);
+    return res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+  return service;
+};
